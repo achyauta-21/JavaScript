c97055d7110d2363dfe2c323191f4c805bd1e527
Achyauta <achyautanandc@gmail.com>
2025-04-12 00:06:35 +0530

    Java Script Variables

---
+++ variable.js
@@ -0,0 +1,37 @@
+// Variables 
+//  *) A variable is the name of a storage location.
+var a = 10;
+console.log(a); 
+
+// Datatypes in JS
+// 1) Primitive data types
+// 1) number,boolean,string,undefined,null,bigint,symbol
+
+console.log("the type of a is",typeof (a));
+
+
+// 1) Numbers IN JS
+//  Positive(14)& negative(-4) 
+//  floating numbers -with decimal(4.6,-8.9)
+//  To find the datatype of the varibale write the typeof (varibale_name)
+
+
+// NAN = The global property is a value representing Not -a Number.
+// 0/0
+// NaN-1
+// NaN*1
+// NaN +NaN
+
+A=0;
+B=0;
+console.log(A/B);
+
+
+//  Operator Precedence
+//  This is the general order of solving an expression  
+// 1) ()
+// 2) **
+// 3) *,/,%
+// 4) +,-
+
+// ex = (5+2)*3 = 9
